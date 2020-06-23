2629aa9218f11929af8255d67a7eb245510f4cc9
IryArkhy <37odri@gmail.com>
2020-06-23 23:27:34 +0300

    DO THE EXERCISE 02: undone

--- 02/index.js
+++ 02/index.js
@@ -4,14 +4,26 @@ const elBox = document.querySelector('#box');
 
 const machine = createMachine({
   // Add your object machine definition here
-});
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        CLICK: 'active'
+      }
+    },
+    active: {
+      on: {
+        CLICK: 'inactive'
+      }
+
+    });
 
 // Change this to the initial state
-let currentState = undefined;
+let currentState = machine.initial;
 
 function send(event) {
   // Determine and update the `currentState`
-
+  currentState = machine.transition(currentState, event);
   elBox.dataset.state = currentState.value;
 }
 
