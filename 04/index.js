4f3dcf6f76b5dad41e134e5abf0d907c260f7f53
IryArkhy <37odri@gmail.com>
2020-06-24 13:11:48 +0300

    exercise 04

--- 04/index.js
+++ 04/index.js
@@ -3,8 +3,14 @@ import { createMachine, interpret } from 'xstate';
 const elBox = document.querySelector('#box');
 
 const setPoint = (context, event) => {
+  const { clientX, clientY } = event;
   // Set the data-point attribute of `elBox`
-  // ...
+  elBox.dataset.point = `{"clientX": ${clientX}, "clientY" ${clientY}}`;
+};
+
+const clearPoint = (context, event) => {
+  // Set the data-point attribute of `elBox`
+  elBox.dataset.point = "Element inactive";
 };
 
 const machine = createMachine({
@@ -14,8 +20,9 @@ const machine = createMachine({
       on: {
         mousedown: {
           // Add your action here
-          // ...
           target: 'dragging',
+          //Execution of the functions in the array is almost immidiate and not always in that order. {context: undefined, event: MouseDown}
+          actions: [setPoint, (context, event) => console.log('context:', context, "event: ", event)],
         },
       },
     },
@@ -23,16 +30,32 @@ const machine = createMachine({
       on: {
         mouseup: {
           target: 'idle',
+          actions: clearPoint
         },
       },
     },
   },
-});
+},
+  {
+    // actions: {
+    //   setPoint: () => console.log(`Overrided setPoint`)
+    // }
+  });
+/*
+object with actions in the end
+
+Now one other thing too, since we could customize this action, this is especially good for testing.
+
+[00:02:48]
+If we want to say that this action side effect is not really going to help us out much when we're unit testing or integration testing this machine, we could say, actions. And then, we could specify that same setPoint action, remember it has to have the same name. And then, we could override this any way we want.
+
+So we could say console.log, overrided setPoint and then you'll see that it no longer shows up here. But we have this overridden however you say it setpoints over here in the console. And so when you're testing, it's a good idea to have these named to actions so that you could replace them with test implementations of your actions instead.
+*/
 
 const service = interpret(machine);
 
 service.onTransition((state) => {
-  console.log(state);
+  // console.log(state);
 
   elBox.dataset.state = state.value;
 });
@@ -42,7 +65,5 @@ service.start();
 elBox.addEventListener('mousedown', (event) => {
   service.send(event);
 });
-
-elBox.addEventListener('mouseup', (event) => {
-  service.send(event);
-});
+// shoerthand for the function above
+elBox.addEventListener('mouseup', service.send);
